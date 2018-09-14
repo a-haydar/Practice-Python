c827e92fbcfc842df663116d388f01f4bf22ea57
A. Haydar <a.haydar.08@gmail.com>
2018-09-14 10:51:17 +0800

    Solve problem 8

---
+++ 008_rock_paper_scissors.py
@@ -0,0 +1,20 @@
+"""Make a two-player Rock-Paper-Scissors game.
+(Hint: Ask for player plays (using input), compare them, print out a message of congratulations to the winner, 
+and ask if the players want to start a new game)
+
+Remember the rules:
+
+Rock beats scissors
+Scissors beats paper
+Paper beats rock"""
+
+while True:
+    hand_1 = input("Player 1, your hand is? ")
+    hand_2 = input("Player 2, your hand is? ")
+    if (hand_1 == "rock" and hand_2 == "scissors") or (hand_1 == "scissors" and hand_2 == "paper") or (hand_1 == "paper" and hand_2 == "rock"):
+        print("Player 1 wins")
+    else:
+        print("Player 2 wins")
+    play_again = input("Play again? [y/n] :")
+    if play_again[0] == "n":
+        break
